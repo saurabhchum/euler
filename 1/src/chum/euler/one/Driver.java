4e2ef30665bd7fb46609697a6dc0e02f8cff760a
saurabh <saurabhcum@gmail.com>
2012-10-09 00:08:00 -0400

    P1 commit.

---
+++ 1/src/chum/euler/one/Driver.java
@@ -0,0 +1,18 @@
+package chum.euler.one;
+
+public class Driver {
+	public final static int RANGE = 1000;
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i < Driver.RANGE; i++) {
+
+			// Very dumb algorithm, just check if i is divisible by 3 or 5 and add to sum
+			if (i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println("Sum: " + sum);
+	}
+
+}
